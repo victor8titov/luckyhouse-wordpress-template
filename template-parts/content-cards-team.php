dd5163c05660f7cb9706bca91158147640020024
w8lt8r <victor8titov@ya.ru>
2019-11-02 18:44:09 +0300

    доработка блока с отзовами

--- template-parts/content-cards-team.php
+++ template-parts/content-cards-team.php
@@ -20,26 +20,24 @@
               ?>
 			<div class="col-sm-4">
 				<div class="team-member">
-					<?php echo get_the_post_thumbnail(get_the_ID(), 'full',array('class'=>'mx-auto rounded-circle')); ?>
+					<?php 
+					$content = strip_tags(get_the_content());
+					if ( empty( $content ) ): ?>
+					<?php echo get_the_post_thumbnail(get_the_ID(), 'full',array('class'=>'rounded-circle no-link')); ?>
+				<?php else: ?>
+				<a class="team-link " data-toggle="modal" href="#teamModal<?php echo get_the_ID(); ?>">
+					<div class="team-hover rounded-circle">
+					<div class="team-hover-content">
+						<i class="fas fa-plus fa-3x"></i>
+					</div>
+					</div>
+					<?php echo get_the_post_thumbnail(get_the_ID(), 'full',array('class'=>'rounded-circle')); ?>
+				</a>
+			  <?php endif; ?>	
 					<h4><?php the_title(); ?></h4>
 					<p class="text-muted"><?php echo lh_get_meta_box('references_subtitle'); ?></p>
-					<!-- <ul class="list-inline social-buttons">
-					<li class="list-inline-item">
-						<a href="#">
-						<i class="fab fa-twitter"></i>
-						</a>
-					</li>
-					<li class="list-inline-item">
-						<a href="#">
-						<i class="fab fa-facebook-f"></i>
-						</a>
-					</li>
-					<li class="list-inline-item">
-						<a href="#">
-						<i class="fab fa-linkedin-in"></i>
-						</a>
-					</li>
-					</ul> -->
+					
+					
 				</div>
 			</div>
 		<?php 
@@ -56,3 +54,47 @@
       </div>
     </div>
   </section>
+  <?php
+	$query = new WP_Query(array(
+		'post_type' => 'references',
+	)); 
+		
+	if( $query->have_posts() ){
+		while( $query->have_posts() ){
+			$query->the_post();
+			$content = strip_tags(get_the_content());
+			if ( empty( $content ) ) continue;
+?>
+	<!-- Modal <?php echo get_the_ID(); ?> -->
+	<div class="portfolio-modal modal fade" id="teamModal<?php echo get_the_ID(); ?>" tabindex="-1" role="dialog" aria-hidden="true">
+					<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="close-modal" data-dismiss="modal">
+						<div class="lr">
+							<div class="rl"></div>
+						</div>
+						</div>
+						<div class="container">
+						<div class="row">
+							<div class="col-lg-8 mx-auto">
+							<div class="modal-body">
+								<!-- Project Details Go Here -->
+								<h2 class="text-uppercase"><?php the_title(); ?></h2>
+								<p class="item-intro text-muted"><?php echo lh_get_meta_box('group_subtitle_article'); ?></p>
+								<?php echo get_the_post_thumbnail(get_the_ID(),'full',array('class'=>'img-fluid d-block mx-auto')); ?>
+								<?php the_content(); ?> 
+								<button class="btn btn-primary" data-dismiss="modal" type="button">
+								<i class="fas fa-times"></i>
+								Close Project</button>
+							</div>
+							</div>
+						</div>
+						</div>
+					</div>
+					</div>
+				</div>
+			<?php
+		}
+		wp_reset_postdata(); // сбрасываем переменную $post
+	} 
+?>
